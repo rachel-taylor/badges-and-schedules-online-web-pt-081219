06deb9b2cf442c7f30c60796c56772a2048446fd
Rachel T <rachmaybe@gmail.com>
2019-08-21 01:52:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+   attendees.map do |name|
+     "Hello, my name is #{name}."
+   end 
+end
+
+def assign_rooms(attendees)
+  attendees.each do |name|
+    puts badge_maker(name)
+  end 
+  assign_rooms(attendees).each do |number|
+    puts number
+  end 
+end 
