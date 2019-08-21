884913b3a2c5e1ce3e3bab64e9dd80138b297ed0
Rachel T <rachmaybe@gmail.com>
2019-08-21 01:57:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,13 @@ def batch_badge_creator(attendees)
    end 
 end
 
+def assign_rooms(attendees)
+  array = []
+  counter = 1 
+  array.each do |name|
+    array.push("Hello, #{name}! You'll be assigned to room #")
+end 
+
 def print(attendees)
   attendees.each do |name|
     puts badge_maker(name)
